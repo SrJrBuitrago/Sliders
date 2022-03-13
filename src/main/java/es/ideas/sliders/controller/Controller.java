8373a4c389303d3362d407b1567bbbccb529fc95
SrJrBuitrago <5588497@alu.murciaeduca.es>
2022-03-13 17:31:17 +0100

    Base del proyecto

---
+++ src/main/java/es/ideas/sliders/controller/Controller.java
@@ -0,0 +1,14 @@
+package es.ideas.sliders.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module es.ideas.sliders {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.ideas.sliders to javafx.fxml;
+    exports es.ideas.sliders;
+    exports es.ideas.sliders.controller;
+    opens es.ideas.sliders.controller to javafx.fxml;
+}
